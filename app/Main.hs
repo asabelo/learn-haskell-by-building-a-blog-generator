15555255eaa7cc4be5e23672489f823cc58c50f8
asabelo <asabelo@proton.me>
2024-02-23 23:38:28 +0100

    Define and compose functions to wrap strings in HTML tags

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
-import Lib
-
 main :: IO ()
-main = someFunc
+main = putStrLn $ makeHtml "My page title" "Hello, world!"
+
+makeHtml :: String -> String -> String
+makeHtml title body = html_ $ (head_.title_ $ title) <> body_ body
+
+html_ :: String -> String
+html_ content = "<html>" <> content <> "</html>"
+
+body_ :: String -> String
+body_ content = "<body>" <> content <> "</body>"
+
+head_ :: String -> String
+head_ content = "<head>" <> content <> "</head>"
+
+title_ :: String -> String
+title_ content = "<title>" <> content <> "</title>"
