b31bb9d8bb1543d21ebc8b740e5c4acc0f705b25
asabelo <asabelo@proton.me>
2024-02-24 10:40:14 +0100

    Extract code to Html module

---
+++ src/Html.hs
@@ -0,0 +1,37 @@
+module Html(
+        Html,
+        Title,
+        Structure,
+        html_,
+        p_,
+        h1_,
+        append_
+    ) where
+
+html_ :: Title -> Structure -> Html
+html_ title structure = 
+    Html $ el "html" 
+        $ el "head" (el "title" title)
+        <> el "body" (show structure)
+
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+type Title = String
+
+newtype Html = Html String
+instance Show Html where
+    show (Html s) = s
+    
+newtype Structure = Structure String 
+instance Show Structure where
+    show (Structure s) = s
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure s1) (Structure s2) = Structure $ s1 <> s2
+
+p_ :: String -> Structure
+p_ = Structure . el "p"
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
