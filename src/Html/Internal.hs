ac48948196c924c8eb5455a5cda4e785b7831806
asabelo <asabelo@proton.me>
2024-02-24 12:01:41 +0100

    Expose Html implementation through Html.Internal

---
+++ src/Html/Internal.hs
@@ -0,0 +1,40 @@
+module Html.Internal(module Html.Internal)
+    where
+
+html_ :: Title -> Structure -> Html
+html_ title structure =
+    Html $ el "html"
+        $ el "head" (el "title" title)
+        <> el "body" (show structure)
+
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+type Title = String
+
+newtype Html = Html String
+instance Show Html where
+    show (Html s) = s
+
+newtype Structure = Structure String
+instance Show Structure where
+    show (Structure s) = s
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure s1) (Structure s2) = Structure $ s1 <> s2
+
+p_ :: String -> Structure
+p_ = Structure . el "p" . escape
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1" . escape
+
+-- Escaping rules from https://stackoverflow.com/a/7382028
+escape :: String -> String
+escape = concatMap $ \char -> case char of
+    '&' -> "&amp;"
+    '<' -> "&lt;"
+    '>' -> "&gt;"
+    '"' -> "&quot;"
+    '\'' -> "&#39;"
+    _ -> [char]
