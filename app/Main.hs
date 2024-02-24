a6fba69811c53ce898fdc2c90e4f97107bc62e4a
asabelo <asabelo@proton.me>
2024-02-24 10:26:09 +0100

    Implement show for Html and Structure and use them

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,32 @@
 module Main (main) where
 
 main :: IO ()
-main = putStrLn $ makeHtml "My page title" "Hello, world!"
+main = print $ html_ "My page title" $ append_ (h1_ "My page title") (p_ "Hello, world!")
 
-makeHtml :: String -> String -> String
-makeHtml title body = html_ $ (head_.title_ $ title) <> body_ (h1_ title <> p_ body)
-
-html_ :: String -> String
-html_ = el "html"
-
-body_ :: String -> String
-body_ = el "body"
-
-head_ :: String -> String
-head_ = el "head"
-
-title_ :: String -> String
-title_ = el "title"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
+html_ :: Title -> Structure -> Html
+html_ title structure = 
+    Html $ el "html" 
+        $ el "head" (el "title" title)
+        <> el "body" (show structure)
 
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
+type Title = String
 
 newtype Html = Html String
-    deriving Show
-
+instance Show Html where
+    show (Html s) = s
+    
 newtype Structure = Structure String 
-    deriving Show
+instance Show Structure where
+    show (Structure s) = s
 
 append_ :: Structure -> Structure -> Structure
 append_ (Structure s1) (Structure s2) = Structure $ s1 <> s2
+
+p_ :: String -> Structure
+p_ = Structure . el "p"
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
