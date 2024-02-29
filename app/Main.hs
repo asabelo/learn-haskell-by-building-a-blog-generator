6b28e52310a8edb3761084dc5f6eb7378437ee8e
asabelo <asabelo@proton.me>
2024-02-29 19:34:09 +0100

    Implement command line behaviour: r/w to stdio or files

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,30 @@
 module Main (main) where
 
-import Html
+import System.Directory ( doesFileExist )
+import System.Environment ( getArgs )
 
-title = "<My> <page> <title>"
-content =  
-    (h_ 1 title)
-    <>
-    (ol_ [ p_ "\"Hello\"", 
-           p_ "&",
-           code_ "\'world\'!" ])
+import Html ( Title )
+import Markup ( parse )
+import Convert ( convert )
+
+process :: Html.Title -> String -> String
+process title = show . convert title . Markup.parse
+
+usage :: String
+usage = "USAGE\n"
+     ++ "\n"
+     ++ "blog-generator \n"
+     ++ "\tReads custom markup from standard input and writes the converted HTML to standard output.\n"
+     ++ "\n"
+     ++ "blog-generator <source file> <destination file>\n"
+     ++ "\tReads custom markup from the file <source file> and writes the converted HTML to the file <destination file>.\n"
 
 main :: IO ()
-main = print $ html_ title content
+main = do
+    args <- getArgs
+    case args of
+        [] -> getContents >>= putStrLn . process ""
+        [srcFile, destFile] -> doesFileExist srcFile 
+                            >>= \exists -> if exists then readFile srcFile >>= writeFile destFile . process "" 
+                                           else error "Source file doesn't exist"
+        _ -> putStrLn usage
